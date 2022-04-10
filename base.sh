1726125ef27b075c21704318182b2ca177fb2076
BnGalal <45380168+BnGalal@users.noreply.github.com>
2022-04-10 09:26:25 +0300

    update user name

--- base.sh
+++ base.sh
@@ -10,9 +10,7 @@ echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:password | chpasswd
-
-# pacman -S  virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak
+echo root:x123 | chpasswd
 
 pacman -S --noconfirm xorg networkmanager network-manager-applet dialog ntfs-3g wireless_tools wpa_supplicant os-prober mtools dosfstools git base-devel linux-headers xf86-video-intel bluez bluez-utils cups alsa-utils pulseaudio pulseaudio-bluetooth grub efibootmgr xdg-utils openssh hplip
 
@@ -20,20 +18,16 @@ pacman -S --noconfirm xorg networkmanager network-manager-applet dialog ntfs-3g 
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 # grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
-grub-install --target=i386-pc /dev/sdX # replace sdx with your disk name, not the partition
+grub-install --target=i386-pc /dev/sda # replace sdx with your disk name, not the partition
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups
-#systemctl enable sshd
-#systemctl enable fstrim.timer
 
-useradd -m xxxuser
-echo xxxuser:password | chpasswd
-usermod -aG libvirt xxxuser
+useradd -m user
+echo user:x123 | chpasswd
 
-echo "xxxuser ALL=(ALL) ALL" >> /etc/sudoers.d/xxxuser
-
+echo "user ALL=(ALL) ALL" >> /etc/sudoers.d/user
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
